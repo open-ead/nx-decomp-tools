ce7f20a2f44f5c8abf75c107fdbec1ea0d9aead4
Léo Lam <leo@leolam.fr>
2021-10-16 19:36:17 +0200

    Avoid overwriting existing names unless a function has been decompiled

--- rename_functions_in_ida.py
+++ rename_functions_in_ida.py
@@ -8,6 +8,32 @@ from util import config
 
 csv_path = config.get_functions_csv_path()
 
+def can_overwrite_name(addr: int, new_name: str):
+    if not new_name or new_name.startswith(("sub_", "nullsub_", "j_")):
+        return False
+
+    old_name: str = idc.get_name(addr)
+    # If we don't have an existing name, then the function can always be renamed.
+    if not old_name:
+        return True
+
+    # Auto-generated names can be overwritten.
+    if old_name.startswith(("sub_", "nullsub_", "j_")):
+        return True
+
+    # If the existing name is mangled, then it probably came from the function list CSV
+    # so it can be overwritten.
+    if old_name.startswith("_Z"):
+        return True
+
+    # Prefer mangled names to temporary names.
+    if new_name.startswith("_Z"):
+        return True
+
+    # Otherwise, we return false to avoid losing temporary names.
+    return False
+
+
 with open(csv_path, "r") as f:
     reader = csv.reader(f)
     # Skip headers
@@ -15,5 +41,5 @@ with open(csv_path, "r") as f:
     for fn in reader:
         addr = int(fn[0], 16)
         name = fn[3]
-        if name and not name.startswith(("sub_", "nullsub_", "j_")):
+        if can_overwrite_name(addr, name):
             idc.set_name(addr, name)
