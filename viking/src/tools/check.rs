377579855931da2e34585653b2becad4e4842d15
MonsterDruide1 <5958456@gmail.com>
2021-11-21 00:14:51 +0100

    `viking`: get `CheckResult` from `check_function`

--- viking/src/tools/check.rs
+++ viking/src/tools/check.rs
@@ -22,7 +22,15 @@ use mimalloc::MiMalloc;
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
-/// Returns false if the program should exit with a failure code at the end.
+enum CheckResult {
+    // If a function does not match, but is marked as such, return this error to show an appropriate exit message.
+    MismatchError,
+    // If a function does match, but is marked as mismatching, return this warning to indicate this and update it if invoked with `--update-matching`.
+    MatchWarn,
+    // Check result matches the expected value listed in the function table.
+    Ok,
+}
+
 fn check_function(
     checker: &FunctionChecker,
     cs: &mut capstone::Capstone,
@@ -30,13 +38,13 @@ fn check_function(
     decomp_elf: &elf::OwnedElf,
     decomp_symtab: &elf::SymbolTableByName,
     function: &functions::Info,
-) -> Result<bool> {
+) -> Result<CheckResult> {
     let name = function.name.as_str();
     let decomp_fn = elf::get_function_by_name(decomp_elf, decomp_symtab, name);
 
     match function.status {
-        Status::NotDecompiled if decomp_fn.is_err() => return Ok(true),
-        Status::Library => return Ok(true),
+        Status::NotDecompiled if decomp_fn.is_err() => return Ok(CheckResult::Ok),
+        Status::Library => return Ok(CheckResult::Ok),
         _ => (),
     }
 
@@ -47,7 +55,7 @@ fn check_function(
             ui::format_symbol_name(name),
             error.to_string().dimmed(),
         ));
-        return Ok(true);
+        return Ok(CheckResult::Ok);
     }
 
     let decomp_fn = decomp_fn.unwrap();
@@ -81,7 +89,7 @@ fn check_function(
                     ),
                 );
                 ui::print_detail_ex(&mut lock, &format!("{}", mismatch));
-                return Ok(false);
+                return Ok(CheckResult::MismatchError);
             }
         }
 
@@ -101,13 +109,14 @@ fn check_function(
                     ui::format_symbol_name(name),
                     function.status.description(),
                 ));
+                return Ok(CheckResult::MatchWarn);
             }
         }
 
         Status::Library => unreachable!(),
     };
 
-    Ok(true)
+    Ok(CheckResult::Ok)
 }
 
 #[cold]
@@ -145,7 +154,7 @@ fn check_all(
                 decomp_symtab,
                 function,
             )?;
-            if !ok {
+            if matches!(ok, CheckResult::MismatchError) {
                 failed.store(true, std::sync::atomic::Ordering::Relaxed);
             }
 
