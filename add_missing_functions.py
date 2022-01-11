0a9d8602aa2dca6a9bdda13df6b5aa2fbc394a09
Léo Lam <leo@leolam.fr>
2022-01-11 01:15:11 +0100

    Allow importing names using add_missing_functions

--- add_missing_functions.py
+++ add_missing_functions.py
@@ -4,7 +4,7 @@ import argparse
 import csv
 import sys
 from pathlib import Path
-from typing import Set, List
+from typing import Dict, Set, List
 
 from util import utils
 
@@ -17,9 +17,13 @@ def main() -> None:
     csv_path = Path(args.csv_path)
 
     known_fn_addrs: Set[int] = {func.addr for func in utils.get_functions(all=True)}
+    names: Dict[int, str] = {func.addr: func.name for func in utils.get_functions(all=True)}
     new_fns: List[utils.FunctionInfo] = []
     for func in utils.get_functions(csv_path, all=True):
-        if func.addr not in known_fn_addrs:
+        if func.addr in known_fn_addrs:
+            if names[func.addr] == "" and not func.name.startswith("_Z"):
+                names[func.addr] = func.name
+        else:
             new_fns.append(func)
 
     new_fn_list: List[utils.FunctionInfo] = []
@@ -31,6 +35,8 @@ def main() -> None:
     writer = csv.writer(sys.stdout, lineterminator="\n")
     writer.writerow("Address,Quality,Size,Name".split(","))
     for func in new_fn_list:
+        if func.addr in names:
+            func.raw_row[3] = names[func.addr]
         writer.writerow(func.raw_row)
 
 
