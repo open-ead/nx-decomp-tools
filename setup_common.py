3f50102f3388f0d49dfc05a9e650d76d73cb80e1
fruityloops1 <fruityloops@littun.co>
2023-08-02 22:33:29 +0200

    Add more clang versions

--- setup_common.py
+++ setup_common.py
@@ -73,7 +73,19 @@ def set_up_compiler(version):
     system = platform.system()
     machine = platform.machine()
 
-    if version == "4.0.1":
+    if version == "3.9.1":
+        builds = {
+            # Linux
+            ("Linux", "x86_64"): {
+                "url": "https://releases.llvm.org/3.9.1/clang+llvm-3.9.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz",
+                "dir_name": "clang+llvm-3.9.1-x86_64-linux-gnu-ubuntu-16.04",
+            },
+            ("Linux", "aarch64"): {
+                "url": "https://releases.llvm.org/3.9.1/clang+llvm-3.9.1-aarch64-linux-gnu.tar.xz",
+                "dir_name": "clang+llvm-3.9.1-aarch64-linux-gnu",
+            }
+        }
+    elif version == "4.0.1":
         builds = {
             # Linux
             ("Linux", "x86_64"): {
@@ -94,6 +106,108 @@ def set_up_compiler(version):
                 "url": "https://releases.llvm.org/4.0.1/clang+llvm-4.0.1-x86_64-apple-darwin.tar.xz",
                 "dir_name": "clang+llvm-4.0.1-x86_64-apple-macosx10.9.0",
             },
+        }
+    elif version == "7.0.0":
+        builds = {
+            # Linux
+            ("Linux", "x86_64"): {
+                "url": "https://releases.llvm.org/7.0.0/clang+llvm-7.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz",
+                "dir_name": "clang+llvm-7.0.0-x86_64-linux-gnu-ubuntu-16.04",
+            },
+            ("Linux", "aarch64"): {
+                "url": "https://releases.llvm.org/7.0.0/clang+llvm-7.0.0-aarch64-linux-gnu.tar.xz",
+                "dir_name": "clang+llvm-7.1.0-aarch64-linux-gnu",
+            },
+
+            # macOS
+            ("Darwin", "x86_64"): {
+                "url": "https://releases.llvm.org/7.0.0/clang+llvm-7.0.0-x86_64-apple-darwin.tar.xz",
+                "dir_name": "clang+llvm-7.0.0-x86_64-apple-darwin",
+            }
+        }
+    elif version == "7.1.0":
+        builds = {
+            # Linux
+            ("Linux", "x86_64"): {
+                "url": "https://github.com/llvm/llvm-project/releases/download/llvmorg-7.1.0/clang+llvm-7.1.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz",
+                "dir_name": "clang+llvm-7.1.0-x86_64-linux-gnu-ubuntu-14.04",
+            },
+            ("Linux", "aarch64"): {
+                "url": "https://github.com/llvm/llvm-project/releases/download/llvmorg-7.1.0/clang+llvm-7.1.0-aarch64-linux-gnu.tar.xz",
+                "dir_name": "clang+llvm-7.1.0-aarch64-linux-gnu",
+            }
+        }
+    elif version == "8.0.0":
+        builds = {
+            # Linux
+            ("Linux", "x86_64"): {
+                "url": "https://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz",
+                "dir_name": "clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04",
+            },
+            ("Linux", "aarch64"): {
+                "url": "https://releases.llvm.org/8.0.0/clang+llvm-8.0.0-aarch64-linux-gnu.tar.xz",
+                "dir_name": "clang+llvm-8.0.0-aarch64-linux-gnu",
+            },
+
+            # macOS
+            ("Darwin", "x86_64"): {
+                "url": "https://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-apple-darwin.tar.xz",
+                "dir_name": "clang+llvm-8.0.0-x86_64-apple-darwin",
+            }
+        }
+    elif version == "9.0.0":
+        builds = {
+            # Linux
+            ("Linux", "x86_64"): {
+                "url": "https://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz",
+                "dir_name": "clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04",
+            },
+            ("Linux", "aarch64"): {
+                "url": "https://releases.llvm.org/9.0.0/clang+llvm-9.0.0-aarch64-linux-gnu.tar.xz",
+                "dir_name": "clang+llvm-9.0.0-aarch64-linux-gnu",
+            },
+
+            # macOS
+            ("Darwin", "x86_64"): {
+                "url": "https://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-darwin-apple.tar.xz",
+                "dir_name": "clang+llvm-9.0.0-x86_64-apple-darwin",
+            }
+        }
+    elif version == "10.0.0":
+        builds = {
+            # Linux
+            ("Linux", "x86_64"): {
+                "url": "https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz",
+                "dir_name": "clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04",
+            },
+            ("Linux", "aarch64"): {
+                "url": "https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-aarch64-linux-gnu.tar.xz",
+                "dir_name": "clang+llvm-10.0.0-aarch64-linux-gnu",
+            },
+
+            # macOS
+            ("Darwin", "x86_64"): {
+                "url": "https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-apple-darwin.tar.xz",
+                "dir_name": "clang+llvm-10.0.0-x86_64-apple-darwin",
+            }
+        }
+    elif version == "11.0.0":
+        builds = {
+            # Linux
+            ("Linux", "x86_64"): {
+                "url": "https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz",
+                "dir_name": "clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04",
+            },
+            ("Linux", "aarch64"): {
+                "url": "https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang+llvm-11.0.0-aarch64-linux-gnu.tar.xz",
+                "dir_name": "clang+llvm-11.0.0-aarch64-linux-gnu",
+            },
+
+            # macOS
+            ("Darwin", "x86_64"): {
+                "url": "https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang+llvm-11.0.0-x86_64-apple-darwin.tar.xz",
+                "dir_name": "clang+llvm-11.0.0-x86_64-apple-darwin",
+            }
         }
     else:
         builds = {}
