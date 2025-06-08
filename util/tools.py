54a6350b97dd6314ee1f6efe9aac2ecbe771f954
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2025-06-08 17:22:30 +0700

    Remove binaries repo submodule and extend tool finding logic

--- util/tools.py
+++ util/tools.py
@@ -1,23 +1,45 @@
 from . config import get_repo_root
 import platform
 import os
+import shutil
 
-def get_tools_bin_dir():
-    path = get_repo_root() / 'tools' / 'common' / 'nx-decomp-tools-binaries'
+def try_find_binaries_repo_tool(tool: str):
+    binaries_repo_path = get_repo_root() / 'toolchain' / 'nx-decomp-tools-binaries'
     system = platform.system()
+    tool_path = str(binaries_repo_path)
     if system == "Linux":
-        return str(path) + "/linux/"
+        tool_path += "/linux/"
     if system == "Darwin":
-        return str(path) + "/macos/"
-    return ""
+        tool_path += "/macos/"
+    tool_path += tool
+    if os.path.isfile(tool_path):
+        return tool_path
+    return None
+
+def try_find_toolchain_tool(tool: str):
+    toolchain_tool_path = get_repo_root() / 'toolchain' / 'bin' / tool
+    if os.path.isfile(toolchain_tool_path):
+        return str(toolchain_tool_path)
+    return None
+
+def try_find_global_tool(tool: str):
+    return shutil.which(tool)
 
 def try_find_external_tool(tool: str):
     return os.environ.get("NX_DECOMP_TOOLS_%s" % tool.upper().replace("-", "_"))
 
 def find_tool(tool: str):
-    tool_from_env = try_find_external_tool(tool)
+    if (tool_from_env := try_find_external_tool(tool)) is not None:
+        return tool_from_env
 
-    if tool_from_env is None:
-        return get_tools_bin_dir() + tool
+    if (tool_from_toolchain := try_find_toolchain_tool(tool)) is not None:
+        return tool_from_toolchain
 
-    return tool_from_env
+    if (tool_from_binaries_repo := try_find_binaries_repo_tool(tool)) is not None:
+        return tool_from_binaries_repo
+
+    if (tool_from_path := try_find_global_tool(tool)) is not None:
+        return tool_from_path
+
+    print(f"Could not find tool: {tool} (maybe install it manually?)")
+    exit(1)
