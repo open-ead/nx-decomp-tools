c8f3754a38b85ac9263132e4f918e5cf05e0e20e
MonsterDruide1 <5958456@gmail.com>
2026-07-12 13:34:38 +0200

    rewrite type copying

--- ida_scripts/import_types.py
+++ ida_scripts/import_types.py
@@ -23,6 +23,172 @@ def is_header_allowed(path: str) -> bool:
 # -------------
 # CODE
 
+IGNORED_TYPES = [
+    "ptrdiff_t",
+    "uintptr_t",
+    "intptr_t",
+    "int8_t",
+    "int16_t",
+    "int32_t",
+    "int64_t",
+    "intmax_t",
+    "uint32_t",
+    "uintmax_t",
+    "int_fast8_t",
+    "int_fast64_t",
+    "int_least8_t",
+    "int_least16_t",
+    "int_least32_t",
+    "int_least64_t",
+    "uint_fast8_t",
+    "uint_fast64_t",
+    "uint_least8_t",
+    "uint_least16_t",
+    "uint_least32_t",
+    "uint_least64_t",
+    "int_fast16_t",
+    "int_fast32_t",
+    "uint_fast16_t",
+    "uint_fast32_t",
+    "float_t",
+    "double_t",
+    "ulong",
+    "wchar_t",
+    "imaxdiv_t",
+    "pid_t",
+    "uid_t",
+    "__builtin_va_list",
+    "va_list",
+    "gid_t",
+    "__locale_struct",
+    "locale_t",
+    "wint_t",
+    "wctype_t",
+    "_IO_FILE",
+    "FILE",
+    "__mbstate_t",
+    "mbstate_t",
+    "tm",
+    "iovec",
+    "ssize_t",
+    "off_t",
+    "_G_fpos64_t",
+    "fpos_t",
+    "cookie_read_function_t",
+    "cookie_write_function_t",
+    "cookie_seek_function_t",
+    "cookie_close_function_t",
+    "div_t",
+    "ldiv_t",
+    "lldiv_t",
+    "_IO_cookie_io_functions_t",
+    "cookie_io_functions_t",
+    "msghdr",
+    "cmsghdr",
+    "ucred",
+    "mmsghdr",
+    "linger",
+    "sockaddr_storage",
+    "in_port_t",
+    "in_addr_t",
+    "in_addr",
+    "sockaddr_in",
+    "in6_addr",
+    "sockaddr_in6",
+    "ipv6_mreq",
+    "ip_opts",
+    "ip_mreq",
+    "ip_mreqn",
+    "ip_mreq_source",
+    "ip_msfilter",
+    "group_req",
+    "group_source_req",
+    "group_filter",
+    "in_pktinfo",
+    "in6_pktinfo",
+    "ip6_mtuinfo",
+    "timespec",
+    "__sigset_t",
+    "sigset_t",
+    "suseconds_t",
+    "timeval",
+    "fd_mask",
+    "fd_set",
+    "Elf32_Half",
+    "Elf64_Half",
+    "Elf32_Word",
+    "Elf32_Sword",
+    "Elf64_Word",
+    "Elf64_Sword",
+    "Elf32_Xword",
+    "Elf32_Sxword",
+    "Elf64_Xword",
+    "Elf64_Sxword",
+    "Elf32_Addr",
+    "Elf64_Addr",
+    "Elf32_Off",
+    "Elf64_Off",
+    "Elf32_Section",
+    "Elf64_Section",
+    "Elf32_Versym",
+    "Elf64_Versym",
+    "Elf32_Ehdr",
+    "Elf64_Ehdr",
+    "Elf32_Shdr",
+    "Elf64_Shdr",
+    "Elf32_Sym",
+    "Elf64_Sym",
+    "Elf32_Syminfo",
+    "Elf64_Syminfo",
+    "Elf32_Rel",
+    "Elf64_Rel",
+    "Elf32_Rela",
+    "Elf64_Rela",
+    "Elf32_Phdr",
+    "Elf64_Phdr",
+    "Elf32_Dyn",
+    "Elf64_Dyn",
+    "Elf32_Verdef",
+    "Elf64_Verdef",
+    "Elf32_Verdaux",
+    "Elf64_Verdaux",
+    "Elf32_Verneed",
+    "Elf64_Verneed",
+    "Elf32_Vernaux",
+    "Elf64_Vernaux",
+    "Elf32_auxv_t",
+    "Elf64_auxv_t",
+    "Elf32_Nhdr",
+    "Elf64_Nhdr",
+    "Elf32_Move",
+    "Elf64_Move",
+    "Elf32_gptab",
+    "Elf32_RegInfo",
+    "Elf_Options",
+    "Elf_Options_Hw",
+    "Elf32_Lib",
+    "Elf64_Lib",
+    "Elf32_Conflict",
+    "wctrans_t",
+    "netent",
+    "hostent",
+    "servent",
+    "protoent",
+    "nfds_t",
+    "pollfd",
+    "size_t",
+    "uint8_t",
+    "uint16_t",
+    "uint64_t",
+    "u32",
+    "u64",
+    "socklen_t",
+    "sa_family_t",
+    "sockaddr",
+    "addrinfo",
+    "time_t",
+]
+
 def adapted_path_wsl(path: str) -> str:
     # if path starts with double backslash, we're calling this from windows using a UNC path (or drive letter, auto-resolved) => convert to WSL path
     if path.startswith("\\\\wsl.localhost"):
@@ -236,99 +402,136 @@ def ida_print_decl(ordinal):
         exit(1)
     return decl.split("\n", 1)[1]
 
-errors = ida_srclang.parse_decls_with_parser_ext("clang", til, file_all, idaapi.HTI_FIL)
-print("Done, parsed %d declarations with %d errors." % (len(list(til.named_types())), errors))
+class IDAProgressReporter:
+    PREFIX = "NODELAY\nHIDECANCEL\n"
+    STEPS = 5
+    def __init__(self):
+        self.message = "Waiting for progress report..."
+        self.total_steps = None
+        self.step = 0
+    
+    def iter(self, iterable, message):
+        l = list(iterable)
+        self.next_step(message, total_steps=len(l))
+        class IterableProgressReporter:
+            def __init__(self, iterable, reporter):
+                self.iterable = iterable
+                self.reporter = reporter
+            def __iter__(self):
+                for i, item in enumerate(self.iterable):
+                    self.reporter.report(i)
+                    yield item
+        return IterableProgressReporter(l, self)
+    
+    def next_step(self, message, total_steps=None):
+        self.message = message
+        self.total_steps = total_steps
+        self.step += 1
+        self.report(0)
 
-print("Backing up old types...")
-types_backup = {}
-for i,t in enumerate(til.named_types()):
-    name = t.get_type_name()
-    main_t = main_til.get_named_type(name)
-    if main_t is None:  # type does not exist yet
-        continue
-    ordinal = main_t.get_ordinal()
-    if ordinal == 0:  # type does not exist yet
-        types_backup[name] = "ERROR: could not fetch type from main til"
-        print("Could not fetch type with name %s from main til" % name)
-        errors += 1
-        continue
-    if main_t.get_forward_type() == 13:  # forward declaration
-        continue
-    types_backup[name] = ida_print_decl(ordinal)
-ordinals = {}
-for i,t in enumerate(main_til.numbered_types()):
-    if t.get_type_name() not in ordinals:
-        ordinals[t.get_type_name()] = []
-    ordinals[t.get_type_name()].append(t.get_ordinal())
+    def report(self, step):
+        if step % 1234 == 0:
+            postfix = f" ({step}/{self.total_steps})" if self.total_steps is not None else ""
+            prefix = f"Step {self.step} / {self.STEPS}:\n"
+            ida_kernwin.replace_wait_box(self.PREFIX + prefix + self.message + postfix)
+    
+    def show(self):
+        ida_kernwin.show_wait_box(self.PREFIX + self.message)
+    def hide(self):
+        ida_kernwin.hide_wait_box()
 
+progress = IDAProgressReporter()
+try:
+    progress.show()
 
-print("Deleting old types...")
-for i,t in enumerate(til.named_types()):
-    if t.get_type_name() in ordinals:
-        for ordinal in ordinals[t.get_type_name()]:
-            ida_typeinf.del_numbered_type(main_til, ordinal)
+    progress.next_step("Parsing declarations from all headers...")
+    errors = ida_srclang.parse_decls_with_parser_ext("clang", til, file_all, idaapi.HTI_FIL)
+    print("Done, parsed %d declarations with %d errors." % (len(list(til.named_types())), errors))
 
-print("Importing types...")
-num_imported = 0
-errors += ida_srclang.parse_decls_with_parser_ext("clang", main_til, file_all, idaapi.HTI_FIL)
+    types_backup = {}
+    for t in progress.iter(til.named_types(), "Backing up old types..."):
+        name = t.get_type_name()
+        if name in IGNORED_TYPES:
+            continue
+        main_t = main_til.get_named_type(name)
+        if main_t is None:  # type does not exist yet
+            continue
+        ordinal = main_t.get_ordinal()
+        if ordinal == 0:  # type does not exist yet
+            types_backup[name] = "ERROR: could not fetch type from main til"
+            print("Could not fetch type with name %s from main til" % name)
+            errors += 1
+            continue
+        if main_t.get_forward_type() == 13:  # forward declaration
+            continue
+        types_backup[name] = ida_print_decl(ordinal)
 
-print("Fixing existing references...")
-for i,t in enumerate(main_til.named_types()):
-    if t.get_type_name() not in ordinals:
-        continue
-    new_ordinal = t.get_ordinal()
-    for orig_ordinal in ordinals[t.get_type_name()]:
-        ida_typeinf.set_type_alias(main_til, orig_ordinal, new_ordinal)
-for i,t in enumerate(main_til.numbered_types()):
-    replaced = ida_typeinf.replace_ordinal_typerefs(main_til, t)
-for i,t in enumerate(main_til.numbered_types()):
-    typename = t.get_type_name()
-    if typename not in ordinals:
-        continue
-    for orig_ordinal in ordinals[typename]:
-        ida_typeinf.del_numbered_type(main_til, orig_ordinal)
+    ordinals = {}
+    for t in progress.iter(main_til.numbered_types(), "Backing up ordinals..."):
+        if ordinals.get(t.get_type_name()) is not None:
+            print("Type with name %s has multiple ordinals in main til" % t.get_type_name())
+            errors += 1
+        ordinals[t.get_type_name()] = t.get_ordinal()
 
-num_new = 0
-num_unchanged = 0
-conflicts = {}
+    for t in progress.iter(til.named_types(), "Moving types from temporary til to main til..."):
+        if t.get_type_name() in IGNORED_TYPES:
+            continue
+        main_t = main_til.get_named_type(t.get_type_name())
+        if t.get_forward_type() == 13 and main_t is not None and main_t.get_forward_type() != 13:  # forward declaration
+            continue
 
-print("Comparing with backup...")
+        if t.get_type_name() in ordinals:  # existing type => replace existing ordinal
+            t.set_numbered_type(main_til, ordinals.get(t.get_type_name(), 0), ida_typeinf.NTF_COPY|ida_typeinf.NTF_REPLACE, t.get_type_name())
+        else:  # new type => do not override existing ordinal
+            t.set_named_type(main_til, t.get_type_name(), ida_typeinf.NTF_COPY|ida_typeinf.NTF_REPLACE)
 
-for i,t in enumerate(til.named_types()):
-    name = t.get_type_name()
-    ordinal = ida_typeinf.get_type_ordinal(main_til, name)
-    if ordinal != 0:
-        newtype = ida_print_decl(ordinal)
-    else:
-        newtype = "ERROR: could not fetch type from main til"
-        errors += 1
-        print("Could not fetch new type with name %s from main til" % name)
-    oldtype = types_backup.get(name)
-    if oldtype is None:
-        if t.get_forward_type() == 13:  # forward declaration
+    num_new = 0
+    num_unchanged = 0
+    num_ignored = 0
+    conflicts = {}
+
+    for t in progress.iter(til.named_types(), "Comparing with backup..."):
+        name = t.get_type_name()
+        if name in IGNORED_TYPES:
+            num_ignored += 1
+            continue
+        ordinal = ida_typeinf.get_type_ordinal(main_til, name)
+        if ordinal != 0:
+            newtype = ida_print_decl(ordinal)
+        else:
+            newtype = "ERROR: could not fetch type from main til"
+            errors += 1
+            print("Could not fetch new type with name %s from main til" % name)
+        oldtype = types_backup.get(name)
+        if oldtype is None:
+            if t.get_forward_type() == 13:  # forward declaration
+                num_unchanged += 1
+            else:
+                num_new += 1
+        elif oldtype == newtype:
             num_unchanged += 1
         else:
-            num_new += 1
-    elif oldtype == newtype:
-        num_unchanged += 1
-    else:
-        conflicts[name] = (oldtype, newtype)
+            conflicts[name] = (oldtype, newtype)
 
-import datetime
-date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-with open(f"type_conflicts-{date}.txt", "w") as f:
-    for name, (oldtype, newtype) in conflicts.items():
-        f.write(f"Conflict for type {name}:\n")
-        f.write(f"Old: {oldtype}\n")
-        f.write(f"New: {newtype}\n")
-        f.write("\n")
+    import datetime
+    date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    with open(f"type_conflicts-{date}.txt", "w") as f:
+        for name, (oldtype, newtype) in conflicts.items():
+            f.write(f"Conflict for type {name}:\n")
+            f.write(f"Old: {oldtype}\n")
+            f.write(f"New: {newtype}\n")
+            f.write("\n")
 
-ida_kernwin.msg(f"""
-Importing types finished.
-  errors: {errors}
-  total types: {len(list(til.named_types()))}
-  imported: {num_imported}
-  new: {num_new}
-  unchanged: {num_unchanged}
-  conflicts: {len(conflicts)} (saved to {os.getcwd()}/type_conflicts-{date}.txt)
-""")
+    ida_kernwin.msg(f"""
+    Importing types finished.
+    errors: {errors}
+    total types: {len(list(til.named_types()))}
+    new: {num_new}
+    unchanged: {num_unchanged}
+    ignored: {num_ignored}
+    conflicts: {len(conflicts)} (saved to {os.getcwd()}/type_conflicts-{date}.txt)
+    """)
+finally:
+    progress.hide()
+
+ida_typeinf.free_til(til)
